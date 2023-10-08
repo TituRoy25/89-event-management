12d91db904a743977dfa179e4fc7346ba9b71e2f
TituRoy25 <tituroydinajpur11@gmail.com>
2023-10-08 13:29:14 +0600

    Banner section modify

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -5,7 +5,7 @@ const Events = ({events}) => {
     console.log(events);
     return (
         <div>
-            <h1 className="text-2xl py-5 text-center">Event Categories</h1>
+            <h1 className="text-2xl font-bold mt-10 text-center">Our Services</h1>
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 py-5">
                 {
--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -10,12 +10,12 @@ const Banner = () => {
         >
 
             <div className="hero-content text-center text-neutral-content">
-                <div className="">
-                    <h1 className="mb-5 text-[#f981cf] text-3xl font-bold">
+                <div>
+                    <h1 className="text-[#ef9ad1] text-4xl font-bold">
                         Event-Management
                     </h1>
 
-                    <div className="flex justify-center items-center mt-5">
+                    {/* <div className="flex justify-center items-center mt-5">
                         <input
                             type="text"
                             placeholder="Search here...."
@@ -24,7 +24,7 @@ const Banner = () => {
                         <button className="btn btn-primary rounded-l-none">
                             Search
                         </button>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
